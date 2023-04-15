b4675172b1410211adb7551d04b9831e6d8c12e2
DatMV <maidat0088@gmail.com>
2023-04-15 23:24:37 +0700

    Change Height

--- src/components/CreateOrderPanel.jsx
+++ src/components/CreateOrderPanel.jsx
@@ -109,7 +109,7 @@ export default function CreateOrderPanel() {
 
   return (
     <Box
-      maxHeight= {showForm ? "650px" : "700px"}
+      maxHeight={showForm ? `${window.innerHeight - 250}px` : `${window.innerHeight - 80}px`} 
       overflow="auto"
       sx={
         mediaMd
